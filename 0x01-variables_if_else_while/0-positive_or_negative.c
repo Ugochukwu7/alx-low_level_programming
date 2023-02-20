fb85a4be8ece4f1cfd4c7f8c8ab0a3812c820700
Ugochukwu7 <ezehugochukwu7@gmail.com>
2023-02-20 11:49:04 -0800

    tasko

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,7 +19,8 @@ n = rand() - RAND_MAX / 2;
 if (n > 0)
 printf("%d is positive", n);
 else if (n == 0)
-printf("%d is zero", n);else
+printf("%d is zero", n);
+else if (n < 0)
 printf("%d is negative", n);
 printf("\n");
 return (0);
