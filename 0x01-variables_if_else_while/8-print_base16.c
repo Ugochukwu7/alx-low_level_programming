0b6fa5add11e746aa2b915d205d9375d86e415df
Ugochukwu7 <ezehugochukwu7@gmail.com>
2023-02-20 15:31:03 -0800

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints base 16 number                                                                           
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+char d = '0';
+
+while (d <= '9')
+{
+putchar(d);
+d++;
+}
+for (c = 'a'; c <= 'f'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
