4894ffa7e27cf7ce46ee5f181a677fe1096732cd
Ugochukwu7 <ezehugochukwu7@gmail.com>
2023-02-21 11:27:42 -0800

    tasko1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,16 +2,15 @@
 /**
  * Description: print _alphabets - function that prints alphabets in lowercase
  *
- * Return: always 0
+ * Return: Always 0;
  */
 
 
-void print_alphabet_x10(void);(
+void print_alphabet_x10(void)
 {
 char i;
 
 for (i = 'a'; i <= 'z'; i++)
 _putchar(i);
 _putchar('\n');
-return (0);
 }
