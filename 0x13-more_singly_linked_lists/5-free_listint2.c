a6de9810859cde7cacdb46bcdd417b406b215cd3
Ugochukwu7 <ezehugochukwu7@gmail.com>
2023-04-05 07:28:57 -0700

    task5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,27 @@
+include "lists.h"
+
+/**
+*free_listint2 - frees a listint_ list
+*@head: pointer to the head of the list
+*Return: the head to NULL.
+*/
+
+void free_listint2(listint_t **head)
+{
+listint_t *tmp; /* Declare a Temp pointer to the head */
+
+if (head == NULL)
+return;
+{
+/* Traverse the list and free each node */
+while (*head != NULL)
+{
+tmp = (*head)->next;
+free(*head); /* free the node */
+*head = tmp; /* Move head to the next node */
+}
+
+head  = NULL; /* set the head to NULL */
+
+}
+}
