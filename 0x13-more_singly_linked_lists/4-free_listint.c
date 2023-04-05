5cf04cfe401d757124e9a265cf11e87a1045af63
Ugochukwu7 <ezehugochukwu7@gmail.com>
2023-04-05 07:25:48 -0700

    task4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list.
+ * @head: pointer of the list
+ * Return: Void
+ */
+
+void free_listint(listint_t *head)
+{
+listint_t *tmp; /* Declare a temp pointer */
+
+while (head)
+{
+tmp = head; /* store current node in temp */
+head = head->next; /* update header pointer to the next node */
+free(tmp); /* free memory alloted for the current node */
+}
+}
