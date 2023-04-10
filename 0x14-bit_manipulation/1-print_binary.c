793d197ebbae9787641c89929c4f42e525bc6b33
Ugochukwu7 <ezehugochukwu7@gmail.com>
2023-04-10 04:53:07 -0700

    task02

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_binary - prints the binary of a number
+ * @n: the number to be printed in binary
+ */
+void print_binary(unsigned long int t)
+{
+	if (t > 1)
+		print_binary(t >> 1);
+	_putchar((t & 1) + '0');
+}
